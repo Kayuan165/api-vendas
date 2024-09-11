11cabcec9fffc5cc028ecf57cfa8141342a7f6bb
Kayuan165 <kayuandiasoli@gmail.com>
2024-09-11 19:24:00 -0300

    Criado o Service para a listagem de todos os produtos

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRpository";
+import AppError from "@shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
