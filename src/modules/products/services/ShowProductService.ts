840f043020887f824c66d42ca8576a1a36d74073
Kayuan165 <kayuandiasoli@gmail.com>
2024-09-11 19:31:21 -0300

    Criado o Service para a Listagem de apenas um produto

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRpository";
+import Product from "../typeorm/entities/Product";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Produto não encontrado!')
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
