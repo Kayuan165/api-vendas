416d876803084e18864a3a785457fbc1b57200f8
Kayuan165 <kayuandiasoli@gmail.com>
2024-09-21 14:34:09 -0300

    Configurado as requisições para resultar o id do usuário pelo Request, com a validação do token

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from "@shared/errors/AppError";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+interface tokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -17,7 +23,13 @@ export default function isAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decodeToken = verify(token, auth.jwt.secret);
+    const decodedToken = verify(token, auth.jwt.secret);
+
+    const { sub } = decodedToken as tokenPayload;
+
+    request.user = {
+      id: sub,
+    }
 
     return next();
   } catch {
