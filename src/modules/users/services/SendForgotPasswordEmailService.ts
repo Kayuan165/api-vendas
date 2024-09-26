654490afae29df4fb2121e5a8802e6a54666fb6d
Kayuan165 <kayuandiasoli@gmail.com>
2024-09-26 20:20:30 -0300

    Finalizado os ajustes para a redefinção de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,9 +1,9 @@
 import { getCustomRepository } from "typeorm";
 import AppError from "@shared/errors/AppError";
-import UserToken from "../typeorm/entities/UserToken";
+import path from "path";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensReposiory";
-import EtherealMail from "@config/mail/etherealmail";
+import EtherealMail from "@config/mail/EtherealMail";
 
 interface IRequest {
   email: string;
@@ -22,7 +22,8 @@ class SendForgotPasswordEmailService {
 
     const { token } = await userTokenRepository.generate(user.id);
 
-    //console.log(token);
+    const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
     await EtherealMail.sendMail({
       to: {
         name: user.name,
@@ -30,11 +31,11 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de Senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
-        }
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
       },
     });
   }
