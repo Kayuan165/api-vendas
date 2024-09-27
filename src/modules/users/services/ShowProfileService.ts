eacb004578f210964bd5623a3705e86fa5af9630
Kayuan165 <kayuandiasoli@gmail.com>
2024-09-27 19:03:00 -0300

    Criado o service de vizualização do perfil do usuário e o service de atualização do perfil de usuário

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/user";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import AppError from "@shared/errors/AppError";
+
+
+interface IRequest {
+  user_id: string;
+}
+
+
+class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
